b5735958bc24f648339c20e482181312f95729c6
zouxiaodong <zouxd@chip-cloud.com>
2020-08-25 14:35:58 +0800

    demo test

--- consumer-demo/src/main/java/com/example/consul/consumer/demo/ctrl/ToolServiceClient.java
+++ consumer-demo/src/main/java/com/example/consul/consumer/demo/ctrl/ToolServiceClient.java
@@ -3,7 +3,7 @@ package com.example.consul.consumer.demo.ctrl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "tool-service")
+@FeignClient(value = "tool-service",name = "tool-service",url = "http://192.168.61.53:8009")
 public interface ToolServiceClient {
 
     @GetMapping("/tools/1")
